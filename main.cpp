6045e025d7a277b7dd01156f19ac0c47d944537f
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-17 14:45:06 +0300

    Add code structure

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include <cmath>
+#include <memory>
+
+float getNumberFromUser(std::string_view msg) {
+    char buff[sizeof(int) * 8];
+    std::cout << msg;
+    std::cin >> buff;
+    return strtof(buff, nullptr);
+}
 
 bool isPrime(unsigned int num) {
     if (num % 2 == 0) {
@@ -14,11 +22,19 @@ bool isPrime(unsigned int num) {
     return true;
 }
 
+
+void fillPrimesArray(std::shared_ptr<int[]> primes_arr, int num_primes) {
+
+}
+
 int main() {
-    unsigned int nums[] = {2, 7, 31, 50, 93, 113};
-    for (auto num: nums) {
-        bool num_is_prime = isPrime(num);
-        std::cout << "isPrime(" << num << ") = " << (num_is_prime ? "True" : "False") << std::endl;
+    int num_primes = (int)getNumberFromUser("Enter number of primes: ");
+    if (num_primes <= 0) {
+        std::cout << "Invalid number of primes, Exiting..." << std::endl;
+        return 1;
     }
+
+    std::shared_ptr<int[]> primes_arr = std::make_shared<int>(num_primes);
+
     return 0;
 }
