20cc294647016609b3488563874f444e45276dc9
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-17 13:44:29 +0300

    Add Multiplication table

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <iomanip>
+
+void printMultiplicationTable(int size) {
+    char sep[] = " ";
+
+    for (int i = 1; i <= size; i++) {
+        for (int j = 1; j <= size; j++) {
+            std::cout << std::setw(3) << std::left << i * j << sep;
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size = 11;
+    std::cout << "Multiplication table up to " << size << ":" << std::endl;
+    printMultiplicationTable(size);
     return 0;
 }
