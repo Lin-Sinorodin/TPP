5feaba30092166de6d3d9adcc1e36277a0e396f0
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-17 16:43:31 +0300

    Refactor code according to CR

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
-#include <cmath>
+#include <list>
+#include "prime.h"
 
-bool isPrime(unsigned int num) {
-    if (num % 2 == 0) {
-        return false;
-    }
-
-    for (int i = 3; i < std::sqrt(num); i+=2) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main() {
-    unsigned int nums[] = {2, 7, 31, 50, 93, 113};
+    std::list<int> nums = {2, 7, 31, 50, 93, 113};
     for (auto num: nums) {
         bool num_is_prime = isPrime(num);
         std::cout << "isPrime(" << num << ") = " << (num_is_prime ? "True" : "False") << std::endl;
