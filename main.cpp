e44af5a5859cd39b2b25ca5b1dfa5378becfa31a
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-17 14:05:00 +0300

    Add isPrime function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cmath>
+
+bool isPrime(unsigned int num) {
+    if (num % 2 == 0) {
+        return false;
+    }
+
+    for (int i = 3; i < std::sqrt(num); i+=2) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    unsigned int nums[] = {2, 7, 31, 50, 93, 113};
+    for (auto num: nums) {
+        bool num_is_prime = isPrime(num);
+        std::cout << "isPrime(" << num << ") = " << (num_is_prime ? "True" : "False") << std::endl;
+    }
     return 0;
 }
