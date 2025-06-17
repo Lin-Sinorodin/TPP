85c6bac947b8ba556b5698452ec1fa5aba727451
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-17 13:24:56 +0300

    Add square root cli app

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <string>
+
+float getNumberFromUser(std::string_view msg) {
+    char buff[sizeof(int) * 8];
+    std::cout << msg;
+    std::scanf("%s", buff);
+    return strtof(buff, nullptr);
+}
+
+int squareRootCLI() {
+    float num = getNumberFromUser("Calculate square root of: ");
+    if (num > 0) {
+        std::cout << "Square root: " << std::sqrt(num) << std::endl;
+        return 0;
+    } else {
+        std::cout << "Invalid input!\n";
+        return 1;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    return squareRootCLI();
 }
