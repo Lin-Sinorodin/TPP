3b082c06662042cc682700ef7d1e1f8cce2df4b1
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-22 16:00:34 +0300

    Handle delete in management

--- main.cpp
+++ main.cpp
@@ -17,8 +17,14 @@ Allocation* ptrToAllocation(void* ptr) {
 }
 
 
+/* Get address to an allocation data structure, and return address to the corresponding pointer from new */
+void* AllocationToPtr(Allocation* allocation) {
+    return (void*)((char*)allocation + sizeof(Allocation));
+}
+
+
 /* Add a new allocation data to the linked list of currently allocated memory */
-void addAllocation(void* ptr, std::size_t size) {
+void newAllocation(void* ptr, std::size_t size) {
     // Modify tail to point to the new node of allocated memory
     Allocation* curr = &allocation;
     while (curr->next) {
@@ -30,6 +36,25 @@ void addAllocation(void* ptr, std::size_t size) {
     Allocation* next = ptrToAllocation(curr->next);
     next->next = nullptr;
     next->size = size;
+}
+
+
+void deleteAllocation(void* ptr) {
+    Allocation* curr = &allocation;
+    if (!curr->next) {
+        return;
+    }
+    Allocation* next = ptrToAllocation(curr->next);
+
+    while (AllocationToPtr(next) != ptr) {
+        curr = next;
+        if (!curr->next) {
+            return;
+        }
+        next = ptrToAllocation(curr->next);
+    }
+
+    curr->next = next->next;
 }
 
 
@@ -52,7 +77,7 @@ void* operator new(std::size_t size) {
 
     if (void *ptr = std::malloc(size + sizeof(Allocation))) {
         void* new_ptr = (void*)((char*)ptr + sizeof(Allocation));
-        addAllocation(new_ptr, size);
+        newAllocation(new_ptr, size);
         return new_ptr;
     }
 
@@ -60,20 +85,32 @@ void* operator new(std::size_t size) {
 }
 
 void operator delete(void* ptr) noexcept {
+    deleteAllocation(ptr);
     std::free(ptrToAllocation(ptr));
 }
 
 
 int main() {
-    int* a = new int;
-    long* b = new long;
-    std::cout << "[debug] Allocated int  at: " << a << std::endl;
-    std::cout << "[debug] Allocated long at: " << b << std::endl;
+    short* a = new short;
+    int*  b = new int;
+    long* c = new long;
+    std::cout << "[debug] Allocated short a at: " << a << std::endl;
+    std::cout << "[debug] Allocated int   b at: " << b << std::endl;
+    std::cout << "[debug] Allocated long  c at: " << c << std::endl;
 
     printAllocations();
 
-    delete(b);
     delete(a);
+    std::cout << "\n[debug] Deleted a" << std::endl;
+    printAllocations();
+
+    delete(c);
+    std::cout << "\n[debug] Deleted c" << std::endl;
+    printAllocations();
+
+    delete(b);
+    std::cout << "\n[debug] Deleted c" << std::endl;
+    printAllocations();
 
     return 0;
 }
