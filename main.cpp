4402dea087eab15897b7df8894dda6988443feb6
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-22 11:31:31 +0300

    Add Q1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void q1(string& str, int key) {for_each(str.begin(), str.end(), [key](char& c) {c ^= static_cast<char>(key);});}
+
+void q1Test(int key) {
+    string s {"hello"};
+    cout << "[Q1] before s = " << s << endl;
+
+    q1(s, key);
+    cout << "[Q1] after  s ^ " << key << " = " << s << endl;
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    q1Test(1);
     return 0;
 }
